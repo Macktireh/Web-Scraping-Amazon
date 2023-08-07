0d2b47fc8d6e2c1da172f732d7146f36e212175d
Macktireh <abdimack97@gmail.com>
2023-08-08 00:33:16 +0200

    Project update

--- amazon_bot.py
+++ amazon_bot.py
@@ -1,91 +1,110 @@
-import time
 import datetime
 import os
-import pprint
-import requests
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from dotenv import load_dotenv
 
 
-# Chargement des variables d'environnement se trouvant dans le fichier .env
 load_dotenv()
 
 
 class AmazonBot:
-
     def __init__(self, mongodb_client, server_smtp):
         self.amazon_header = {
-            'authority': 'www.amazon.fr',
-            'cache-control': 'max-age=0',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'sec-gpc': '1',
-            'sec-fetch-site': 'none',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-user': '?1',
-            'sec-fetch-dest': 'document',
-            'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+            "authority": "www.amazon.fr",
+            "cache-control": "max-age=0",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",  # noqa: E501
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",  # noqa: E501
+            "sec-gpc": "1",
+            "sec-fetch-site": "none",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-user": "?1",
+            "sec-fetch-dest": "document",
+            "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
         }
         self.mongodb_client = mongodb_client
         self.server_smtp = server_smtp
         # Ajouter ici le chemin vers chromedriver
-        self.driver = webdriver.Chrome(executable_path="chromedriver")
+        # self.driver = webdriver.Chrome(executable_path="chromedriver.exe")
+        self.driver = webdriver.Chrome()
 
     def get_product_title(self, soup):
         try:
-            return soup.find('span', {'id': 'productTitle'}).get_text().strip()
-        except:
+            return soup.find("span", {"id": "productTitle"}).get_text().strip()
+        except Exception:
             return None
 
     def get_product_rating(self, soup):
         try:
-            div_avg_cust_reviews = soup.find(
-                'div', {'id': 'averageCustomerReviews'})
-            rating = div_avg_cust_reviews.find(
-                'span', {'class': 'a-icon-alt'}).get_text().strip().split()[0]
-            return float(rating.replace(',', '.'))
-        except:
+            div_avg_cust_reviews = soup.find("div", {"id": "averageCustomerReviews"})
+            rating = (
+                div_avg_cust_reviews.find("span", {"class": "a-icon-alt"})
+                .get_text()
+                .strip()
+                .split()[0]
+            )
+            return float(rating.replace(",", "."))
+        except Exception:
             return None
 
     def get_product_nb_reviewers(self, soup):
         try:
-            nb_reviewers = soup.find(
-                'span', {'id': 'acrCustomerReviewText'}).get_text().strip()
-            return int(''.join(nb_reviewers.split()[:-1]))
-        except:
+            nb_reviewers = (
+                soup.find("span", {"id": "acrCustomerReviewText"}).get_text().strip()
+            )
+            return int("".join(nb_reviewers.split()[:-1]))
+        except Exception:
             return None
 
     def get_product_price(self, soup):
         try:
-            price = soup.find(
-                'span', {'id': 'priceblock_ourprice'}).get_text().strip()
-            return float(price.split()[0].replace(',', '.'))
-        except:
+            price = soup.find("span", {"id": "priceblock_ourprice"}).get_text().strip()
+            return float(price.split()[0].replace(",", "."))
+        except Exception:
             try:
-                price = soup.find('span', {
-                                  'class': 'a-size-medium a-color-price header-price a-text-normal'}).get_text().strip()
-                return float(price.split()[0].replace(',', '.'))
-            except:
+                price = (
+                    soup.find(
+                        "span",
+                        {
+                            "class": "a-size-medium a-color-price header-price a-text-normal"  # noqa: E501
+                        },
+                    )
+                    .get_text()
+                    .strip()
+                )
+                return float(price.split()[0].replace(",", "."))
+            except Exception:
                 try:
-                    price = soup.find('span', {
-                                      'class': 'a-size-base a-color-price header-price a-text-normal'}).get_text().strip()
-                    return float(price.split()[0].replace(',', '.'))
-                except:
+                    price = (
+                        soup.find(
+                            "span",
+                            {
+                                "class": "a-size-base a-color-price header-price a-text-normal"  # noqa: E501
+                            },
+                        )
+                        .get_text()
+                        .strip()
+                    )
+                    return float(price.split()[0].replace(",", "."))
+                except Exception:
                     try:
-                        price = soup.find('span', {'class': 'a-price-whole'}).get_text(
-                        ) + soup.find('span', {'class': 'a-price-fraction'}).get_text()
-                        return float(price.split()[0].replace(',', '.'))
-                    except:
+                        price = (
+                            soup.find("span", {"class": "a-price-whole"}).get_text()
+                            + soup.find(
+                                "span", {"class": "a-price-fraction"}
+                            ).get_text()
+                        )
+                        return float(price.split()[0].replace(",", "."))
+                    except Exception:
                         return None
 
     def get_product_data(self, product_url):
         self.driver.get(product_url)
-        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
-        #r = requests.get(product_url, headers=self.amazon_header)
-        #soup = BeautifulSoup(r.content, 'html.parser')
+        soup = BeautifulSoup(self.driver.page_source, "html.parser")
+        # r = requests.get(product_url, headers=self.amazon_header)
+        # soup = BeautifulSoup(r.content, 'html.parser')
         title = self.get_product_title(soup)
         rating = self.get_product_rating(soup)
         nb_reviewers = self.get_product_nb_reviewers(soup)
@@ -97,104 +116,123 @@ class AmazonBot:
             "rating": rating,
             "nb_reviewers": nb_reviewers,
             "price": price,
-            "update_date": datetime.datetime.now()
+            "update_date": datetime.datetime.now(),
         }
 
     def scrap_urls(self):
         while True:
             # Query MongoDB pour récupérer les liens à scraper
-            product_urls = self.mongodb_client["amazon"]["product_urls"].find({
-                "$or": [
-                    {"updated_at": None},
-                    # On ne scrape un document que 5 minutes après l'avoir déja fait
-                    # Vous pouvez augmenter cette valeur biensur
-                    # Il vaut mieux éviter de spam Amazon de requetes sinon il risque de bloquer votre ip
-                    {"updated_at": {"$lte": datetime.datetime.now() - \
-                                    datetime.timedelta(minutes=2)}}
-                ]
-            })
+            product_urls = self.mongodb_client["amazon"]["product_urls"].find(
+                {
+                    "$or": [
+                        {"updated_at": None},
+                        # On ne scrape un document que 5 minutes après l'avoir déja fait
+                        # Vous pouvez augmenter cette valeur biensur
+                        # Il vaut mieux éviter de spam Amazon de requetes sinon il risque de bloquer votre ip  # noqa: E501
+                        {
+                            "updated_at": {
+                                "$lte": datetime.datetime.now()
+                                - datetime.timedelta(minutes=2)
+                            }
+                        },
+                    ]
+                }
+            )
 
             for product_url in product_urls:
                 print("Url à scraper:", product_url["url"], "\n")
                 data = self.get_product_data(product_url["url"])
+                print(data)
 
                 # Upsert
                 self.mongodb_client["amazon"]["product_data"].update(
-                    {"url": product_url['url']}, {"$set": data}, upsert=True)
+                    {"url": product_url["url"]}, {"$set": data}, upsert=True
+                )
                 # Update
-                self.mongodb_client["amazon"]["product_urls"].update({"url": product_url["url"]}, {"$set": {
-                    'updated_at': datetime.datetime.now()
-                }})
+                self.mongodb_client["amazon"]["product_urls"].update(
+                    {"url": product_url["url"]},
+                    {"$set": {"updated_at": datetime.datetime.now()}},
+                )
 
                 # Dernier prix enregistrer pour un produit
                 try:
-                    last_product_price = self.mongodb_client["amazon"]["product_prices"].find(
-                        {"url": data['url']}).sort([('created_at', -1)]).next()
-                except:
+                    last_product_price = (
+                        self.mongodb_client["amazon"]["product_prices"]
+                        .find({"url": data["url"]})
+                        .sort([("created_at", -1)])
+                        .next()
+                    )
+                except Exception:
                     last_product_price = None
 
-                # On insert directement si aucun prix n'existe pour le produit en question
+                # On insert directement si aucun prix n'existe pour le produit en question  # noqa: E501
                 if last_product_price is None:
                     # Insert
-                    self.mongodb_client["amazon"]["product_prices"].insert({
-                        "url": product_url["url"],
-                        "price": data["price"],
-                        "created_at": datetime.datetime.now()
-                    })
-                # Si il existe un précédent prix au produit et que celui-ci est différent
+                    self.mongodb_client["amazon"]["product_prices"].insert(
+                        {
+                            "url": product_url["url"],
+                            "price": data["price"],
+                            "created_at": datetime.datetime.now(),
+                        }
+                    )
+                # Si il existe un précédent prix au produit et que celui-ci est différent  # noqa: E501
                 # du prix que l'on vient de récuperer
-                elif last_product_price is not None and last_product_price['price'] != data['price']:
+                elif (
+                    last_product_price is not None
+                    and last_product_price["price"] != data["price"]
+                ):
                     # Insert
-                    self.mongodb_client["amazon"]["product_prices"].insert({
-                        "url": product_url["url"],
-                        "price": data["price"],
-                        "created_at": datetime.datetime.now()
-                    })
+                    self.mongodb_client["amazon"]["product_prices"].insert(
+                        {
+                            "url": product_url["url"],
+                            "price": data["price"],
+                            "created_at": datetime.datetime.now(),
+                        }
+                    )
 
                     # On check le type
-                    if (type(data["price"]) is int or type(data['price']) is float) and \
-                            (type(last_product_price["price"]) is int or type(last_product_price["price"]) is float):
-
+                    if (
+                        type(data["price"]) is int or type(data["price"]) is float
+                    ) and (
+                        type(last_product_price["price"]) is int
+                        or type(last_product_price["price"]) is float
+                    ):
                         # On calcule la différence entre l'ancien et le nouveau prix
                         diff_price_percentage = (
-                            1 - data["price"] / last_product_price["price"]) * 100
+                            1 - data["price"] / last_product_price["price"]
+                        ) * 100
 
                         # S'il y a une baisse du prix on envoie un email
                         if diff_price_percentage > 0:
                             message = """
 
-                            Tout d'abord bonjour et j'espère que tu vas bien.
+Bonjour,
 
-                            Bonne affaire
+j'espère que tu vas bien.
 
-                            Diminution du prix de %s%% pour le produit %s.
-                            Précédent prix: %s.
-                            Nouveau prix: %s.
+Offre Sensuelle :
 
-                            Bien Cordialement,
-                            
+Succombez à la tentation avec une réduction de %s%% sur notre irrésistible produit %s.
+Prix précédent : %s.
+Nouveau prix : %s.
+
+
+Cordialement,
+Amazon Bot
+
                             """ % (
                                 diff_price_percentage,
                                 product_url["url"],
                                 last_product_price["price"],
-                                data["price"]
+                                data["price"],
                             )
-                            # On encode ici le message car j'y ai ajouté un caractére spécial le %
+                            # On encode ici le message car j'y ai ajouté un caractére spécial le %  # noqa: E501
                             message = message.encode("utf-8")
 
                             # Ajouter l'email d'envoi et de réception du message
 
-                            SENDER_EMAIL_ADDRESS = "projet.webscraping.amazonw01@gmail.com"
-                            RECIPIENT_EMAIL_ADDRESS = [
-                                "projet.webscraping.amazonw01@gmail.com",
-                                "mahamoudabdek@gmail.com",
-                                "adensaidosman99@gmail.com",
-                                "asmaabdihassan@gmail.com",
-                                "abdoulazizzongo0@gmail.com",
-                                "jsetondji@gmail.com",
-                                "samadnureye@gmail.com",
-                                "roki.youssouf@gmail.com",
-                                "filan.aden@gmail.com"
-                            ]
+                            EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS")
+                            RECIPIENTS_EMAIL_ADDRESS = os.environ.get("RECIPIENTS_EMAIL_ADDRESS").split(";")  # noqa: E501
                             self.server_smtp.sendmail(
-                                SENDER_EMAIL_ADDRESS, RECIPIENT_EMAIL_ADDRESS, message)
+                                EMAIL_ADDRESS, RECIPIENTS_EMAIL_ADDRESS, message
+                            )
--- main.py
+++ main.py
@@ -1,40 +1,56 @@
-# Import des librairies
 import os
-import smtplib, ssl
+import smtplib
+import ssl
+
 from dotenv import load_dotenv
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
 from amazon_bot import AmazonBot
 
 # Chargement des variables d'environnement se trouvant dans le fichier .env
 load_dotenv()
 
 # Connexion à la base de donnée MongoDB
+# try:
+#     client = MongoClient("mongodb://localhost:27017/")
+#     client.server_info()
+# except Exception as e:
+#     raise e
+
+uri = os.getenv("MONGODB_URI")
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
 try:
-    client = MongoClient('mongodb://localhost:27017/')
-    client.server_info()
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
-    raise e
+    print(e)
 
 # Connexion au serveur pour l'envoi d'email
 try:
     context = ssl.create_default_context()
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context)
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context)
     server.login(os.getenv("SENDER_EMAIL"), os.getenv("EMAIL_PASSWORD"))
 except Exception as e:
     raise e
 
 # Initialisation de la classe AmazonBot
-bot = AmazonBot(mongodb_client=client, server_smtp=server)
+bot = AmazonBot(mongodb_client=client, server_smtp=None)
 # Lancement du Scraping
 bot.scrap_urls()
 
 # Liste des produits Amazon
 
-#product_urls = [
+# product_urls = [
 #    "https://www.amazon.fr/Apple-AirPods-bo%C3%AEtier-charge-Dernier/dp/B07PZR3PVB/ref=sr_1_5?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=airpod&qid=1610634324&sr=8-5",
 #    "https://www.amazon.fr/PlayStation-%C3%89dition-Standard-DualSense-Couleur/dp/B08H93ZRK9/ref=sr_1_1?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=OC9DXXI5NXTR&dchild=1&keywords=playstation+5&qid=1610634362&sprefix=playstatio%2Caps%2C204&sr=8-1",
 #    "https://www.amazon.fr/Nintendo-Switch-avec-paire-Rouge/dp/B07WKNQ8JT/ref=zg_bs_videogames_home_1?_encoding=UTF8&psc=1&refRID=NXGE6WDBSEDAVF5CS2AA",
 #    "https://www.amazon.fr/T12S-Transformation-semaines-minutes-d%C3%A9finitivement/dp/2035999642/ref=zg_bsnr_books_home_1?_encoding=UTF8&psc=1&refRID=71P5R2S30WR2EPDRPAP7",
 #    "https://www.amazon.fr/chronique-Bridgerton-Tomes-Julia-Quinn/dp/2290254738/ref=zg_bsnr_books_home_3?_encoding=UTF8&psc=1&refRID=71P5R2S30WR2EPDRPAP7",
 #    "https://www.amazon.fr/Incorra-Pieuvre-Reversible-Peluche-Cr%C3%A9atifs/dp/B08NW2RRXJ/ref=zg_bsnr_toys_home_3?_encoding=UTF8&psc=1&refRID=71P5R2S30WR2EPDRPAP7"
-#]
+# ]
